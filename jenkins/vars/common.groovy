1f4e793b78be102176c54d96d451d3a96f4256e7
hugoprobe <hugobro9@gmail.com>
2022-07-07 21:36:53 +0700

    Update common.groovy

--- jenkins/vars/common.groovy
+++ jenkins/vars/common.groovy
@@ -24,4 +24,17 @@ def methodA(){
 def methodB(){
         bat  " echo Method BBBC is execute"
 }
+
+def activeChoiceParameter(it){
+    def setStrScript={str-> return [classpath: [], sandbox: true, script:(str)]}
+    return [ $class: 'ChoiceParameter', name: (it.name),  choiceType: (it.type), omitValueField:(it?.omit?:true), description:(it?.desc?:''), script: [ $class: 'GroovyScript', script: (setStrScript(it.script)), fallbackScript:(setStrScript('return "error script"'))]]
+}
+def activeChoiceReactiveParameter(it){
+    def setStrScript={str-> return [classpath: [], sandbox: true, script:(str)]}
+    return [ $class: 'CascadeChoiceParameter', name: (it.name),  choiceType: (it.type), omitValueField:(it?.omit?:true), description:(it?.desc?:''), referencedParameters:(it?.references?:'') ,script: [ $class: 'GroovyScript', script: (setStrScript(it.script)), fallbackScript:(setStrScript('return "error script"'))]]
+}
+def activeChoiceReactiveReferenceParameter(it){
+    def setStrScript={str-> return [classpath: [], sandbox: true, script:(str)]}
+    return [ $class: 'DynamicReferenceParameter', name: (it.name),  choiceType: (it.type), omitValueField:(it?.omit?:true), description:(it?.desc?:''), referencedParameters:(it?.references?:'') ,script: [ $class: 'GroovyScript', script: (setStrScript(it.script)), fallbackScript:(setStrScript('return "error script"'))]]
+}
 return this
