feb43f39cb77e811bdc530618051d583027d5628
hugoprobe <hugobro9@gmail.com>
2022-07-13 15:47:08 +0700

    Update util.groovy

--- jenkins/vars/util.groovy
+++ jenkins/vars/util.groovy
@@ -11,3 +11,19 @@ def deepmkdir(String path)
         }
         makedir(path)
 }
+
+//For getting environment variables via batch
+def getBatchEnv(String strScript="", List<String> args)
+{
+    def result=[:]
+    args="%"+args.join("%:%")+"%:null"
+    echo "args $args"
+    def getVariables  = ''' @echo off
+                        ''' + strScript + '''
+                            echo ''' + args
+    def vars = bat(script:getVariables, returnStdout:true)
+    def var_array = vars.trim().split(":")
+    for(int i=0; i<args.size();i++)
+        result[args[i]]=var_array[i]
+    return result 
+}
