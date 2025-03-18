2d8164a7d076641c977d57ee40d5a34770d60098
hugo.gumelar <hugo.gumelar@gameloft.com>
2025-03-18 14:46:47 +0700

    gasdgasdg

--- jenkins/test.groovy
+++ jenkins/test.groovy
@@ -7,7 +7,7 @@ pipeline {
         ELIGIBLE_NODES          ='JOGBLD0006,JOGBLD0010,JOGBLD0020,JOGBLD0021'
         PUBLISH_DIRECTORIES     ='A,B'
         SCM_URL                 = "${String.valueOf(scm.getUserRemoteConfigs()[0].getUrl())}"
-        GIT_BRANCH              = """${String.valueOf(scm.branches[0].name).replaceFirst(/^refs\/(heads|remotes\/[^\/]+)\/?/, "")}""""
+        GIT_BRANCH              = """${String.valueOf(scm.branches[0].name).replaceFirst(/^refs\/(heads|remotes\/[^\/]+)\/?/, "")}"""
     }
     stages {
         stage("Nodes Allocation & Verification")
