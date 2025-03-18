05d4dc459134e659aaf8e68fd0c1d3a46435af62
hugo.gumelar <hugo.gumelar@gameloft.com>
2025-03-18 16:17:51 +0700

    Test

--- jenkins/test.groovy
+++ jenkins/test.groovy
@@ -2,7 +2,7 @@
 NODES=[:]
 
 pipeline {
-
+    agent none
     environment{
         ELIGIBLE_NODES          ='JOGBLD0006,JOGBLD0010,JOGBLD0020,JOGBLD0021'
         PUBLISH_DIRECTORIES     ='A,B'
@@ -14,8 +14,8 @@ pipeline {
         {
             steps {
                 script{
-                    echo "SCM_URL ___test2_ = $SCM_URL"
-                    echo "GIT_BRANCH ___test2_ = $GIT_BRANCH" 
+                    echo "SCM_URL = $SCM_URL"
+                    echo "GIT_BRANCH = $GIT_BRANCH" 
                 }
                 
             }
