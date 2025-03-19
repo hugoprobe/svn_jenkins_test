37c133a06571046def4c192154c70350adfbae0f
hugoprobe <hugobro9@gmail.com>
2025-03-20 05:05:32 +0700

    Update param.groovy

--- jenkins/vars/param.groovy
+++ jenkins/vars/param.groovy
@@ -48,22 +48,6 @@ def activeChoiceReactiveReference(Map it){
            ]
 }
 
-@NonCPS def table(str,arg=""){ return """<table $arg >$str\n</table>\n"""}
-@NonCPS def th(str, arg="") {return """<th $arg>$str</th>\n"""}
-@NonCPS def tr(str) {return "<tr>$str\n</tr>\n"} 
-@NonCPS def td(str) {return "<td>$str</td>\n"}
-
-@NonCPS
-def getAgentsTableScriptHTML(Map Agents, String[] ELIGIBLE_AGENTS){ 
-    def html_node_options   = {default_label-> """<select name="value" >""" + ([default_label]+ ( (ELIGIBLE_AGENTS.collect { it.trim() }) - default_label)).collect{ item -> return "<option value=$item>$item "}.join(' ') + "</select>"}
-    def html_workdir        = {default_workdir-> """<input type="text" name="value" style="width:100%" value="${default_workdir}" >"""}
-    def html_table_title    = tr(th("") + th("NodeLabel") + th("WorkingDirectory",'style="width:75%"'))
-    def html_table_rows     = ""
-    traverseAgents(Agents, "") { variants, _agent ->
-        html_table_rows += tr(td("${variants}")+td(html_node_options(_agent.label)) + td(html_workdir(_agent.workdir)) )
-    }
-    return table(html_table_title+html_table_rows);
-}
 
 // Function to traverse the map tree recursively
 @NonCPS
