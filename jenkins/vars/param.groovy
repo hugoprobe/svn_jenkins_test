4ee942c6973cddd46f01acb0419686ea6a92c664
hugoprobe <hugobro9@gmail.com>
2025-03-20 05:55:43 +0700

    Update param.groovy

--- jenkins/vars/param.groovy
+++ jenkins/vars/param.groovy
@@ -2,12 +2,6 @@ def isRemoteBranchExistR(String gitRemote, String branchName){
 	def error_status = bat(returnStatus:true, script: "git ls-remote --exit-code --heads ${gitRemote} ${branchName}")
 	return (error_status != 0)
 }
-def test(){
-	return true
-}
-def agentsTable(Map it){
-	return true
-}
 
 def traverseAgents(Map agents, String agentVariant, Closure closure) {
     agents.each { key, value ->
