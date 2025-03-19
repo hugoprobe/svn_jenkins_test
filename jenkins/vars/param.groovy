2a31a3b51e9d138ba20863b364752c47a86ae624
hugoprobe <hugobro9@gmail.com>
2025-03-20 04:33:39 +0700

    Update param.groovy

--- jenkins/vars/param.groovy
+++ jenkins/vars/param.groovy
@@ -33,6 +33,7 @@ def activeChoiceReactive(Map it){
            ]
 }
 
+@NonCPS
 def activeChoiceReactiveReference(Map it){
     def setStrScript={str-> return [classpath: [], sandbox: true, script:(str)]}
     return [  $class: 'DynamicReferenceParameter', 
@@ -78,14 +79,14 @@ def traverseAgents(Map agents, String agentVariant, Closure closure) {
     }
 }
 
-
+@NonCPS
 def agentsTable(String paramName, Map Agents, String[] eligibleNodes, String flattenedAgentsParam=""){
     def flattenedArray = flattenedAgentsParam?.split(',')?:[]
     def flattenedIdx = 0
     traverseAgents(Agents, "") { variants, _agent ->
         _agent.label      = (flattenedIdx>=flattenedArray.size())?:flattenedArray[flattenedIdx++]
         _agent.workdir    = (flattenedIdx>=flattenedArray.size())?:flattenedArray[flattenedIdx++]
-        //println "${variants}: label ${_agent.label} workdir: ${_agent.workdir}"
+        bat "${variants}: label ${_agent.label} workdir: ${_agent.workdir}"
         if(!(_agent?.workdir) )
             error("Workdir at Agent $_agent CANNOT BE NULL. Stopping the build early...")        
     }
