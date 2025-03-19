cd2b3574b246a183fea000e20d8ba8e5f7630675
hugoprobe <hugobro9@gmail.com>
2025-03-20 06:36:33 +0700

    Update param.groovy

--- jenkins/vars/param.groovy
+++ jenkins/vars/param.groovy
@@ -1,8 +1,3 @@
-def isRemoteBranchExistR(String gitRemote, String branchName){
-	def error_status = bat(returnStatus:true, script: "git ls-remote --exit-code --heads ${gitRemote} ${branchName}")
-	return (error_status != 0)
-}
-
 def traverseAgents(Map agents, String agentVariant, Closure closure) {
     agents.each { key, value ->
         def newAgentVariant = agentVariant?"${agentVariant}.${key}":key
@@ -14,6 +9,37 @@ def traverseAgents(Map agents, String agentVariant, Closure closure) {
     }
 }
 
+def activeChoiceReactiveReference(Map it){
+    def setStrScript={str-> return [classpath: [], sandbox: true, script:(str)]}
+    return [  $class: 'DynamicReferenceParameter', 
+              name: (it.name),  
+              choiceType: (it.type), 
+              omitValueField:(it?.omit?:true), 
+              description:(it?.desc?:''), 
+              referencedParameters:(it?.references?:''),
+              script: [ $class: 'GroovyScript', 
+                        script: (setStrScript(it.script)), 
+                        fallbackScript:(setStrScript('return "error script"'))
+                      ]
+           ]
+}
+
+def table(str,arg=""){ return """<table $arg >$str\n</table>\n"""}
+def th(str, arg="") {return """<th $arg>$str</th>\n"""}
+def tr(str) {return "<tr>$str\n</tr>\n"} 
+def td(str) {return "<td>$str</td>\n"}
+
+
+def getAgentsTableScriptHTML(Map Agents, List ELIGIBLE_AGENTS){ 
+    def html_node_options   = {default_label-> """<select name="value" >""" + ([default_label]+ ( (ELIGIBLE_AGENTS.collect { it.trim() }) - default_label)).collect{ item -> return "<option value=$item>$item "}.join(' ') + "</select>"}
+    def html_workdir        = {default_workdir-> """<input type="text" name="value" style="width:100%" value="${default_workdir}" >"""}
+    def html_table_title    = tr(th("") + th("NodeLabel") + th("WorkingDirectory",'style="width:75%"'))
+    def html_table_rows     = ""
+    traverseAgents(Agents, "") { variants, _agent ->
+        html_table_rows += tr(td("${variants}")+td(html_node_options(_agent.label)) + td(html_workdir(_agent.workdir)) )
+    }
+    return table(html_table_title+html_table_rows);
+}
 
 def agentsTable(Map it){	
     def flattenedArray = it.flattenedAgentsParam.split(',')
@@ -27,4 +53,5 @@ def agentsTable(Map it){
         if(!_agent.workdir || _agent.workdir?.trim().isEmpty())
             error("Workdir at Agent $_agent CANNOT BE NULL. Stopping the build early...")        
     }
+	return activeChoiceReactiveReference ([  name:(it.paramName), type:'ET_FORMATTED_HTML',   script:'return """' +  getAgentsTableScriptHTML(it.Agents, it.eligibleNodes) + '"""'])
 }
