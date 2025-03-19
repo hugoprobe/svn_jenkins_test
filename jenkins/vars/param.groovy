085e0d7fbdeab02846935a974b18752804c29470
hugoprobe <hugobro9@gmail.com>
2025-03-20 05:48:55 +0700

    Update param.groovy

--- jenkins/vars/param.groovy
+++ jenkins/vars/param.groovy
@@ -2,3 +2,6 @@ def isRemoteBranchExistR(String gitRemote, String branchName){
 	def error_status = bat(returnStatus:true, script: "git ls-remote --exit-code --heads ${gitRemote} ${branchName}")
 	return (error_status != 0)
 }
+def test(){
+	return true
+}
