9024f43b7a5d9831a5f01ecb584afecfeaa8571c
hugoprobe <hugobro9@gmail.com>
2025-03-20 04:43:28 +0700

    Update param.groovy

--- jenkins/vars/param.groovy
+++ jenkins/vars/param.groovy
@@ -32,7 +32,6 @@ def activeChoiceReactive(Map it){
                       ]
            ]
 }
-
 @NonCPS
 def activeChoiceReactiveReference(Map it){
     def setStrScript={str-> return [classpath: [], sandbox: true, script:(str)]}
@@ -86,13 +85,12 @@ def agentsTable(String paramName, Map Agents, String[] eligibleNodes, String fla
     traverseAgents(Agents, "") { variants, _agent ->
         _agent.label      = (flattenedIdx>=flattenedArray.size())?:flattenedArray[flattenedIdx++]
         _agent.workdir    = (flattenedIdx>=flattenedArray.size())?:flattenedArray[flattenedIdx++]
-        
+        //println "${variants}: label ${_agent.label} workdir: ${_agent.workdir}"
         if(!(_agent?.workdir) )
             error("Workdir at Agent $_agent CANNOT BE NULL. Stopping the build early...")        
     }
 
     return activeChoiceReactiveReference ([  name:(paramName), type:'ET_FORMATTED_HTML',   script:'return """' +  getAgentsTableScriptHTML(Agents, eligibleNodes) + '"""'])
 }
-
 
 
