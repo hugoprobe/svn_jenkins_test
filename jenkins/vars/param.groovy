382e1fa661335a2046f6cd273c949826ab06a962
hugoprobe <hugobro9@gmail.com>
2025-03-20 05:53:18 +0700

    Update param.groovy

--- jenkins/vars/param.groovy
+++ jenkins/vars/param.groovy
@@ -8,3 +8,27 @@ def test(){
 def agentsTable(Map it){
 	return true
 }
+
+def traverseAgents(Map agents, String agentVariant, Closure closure) {
+    agents.each { key, value ->
+        def newAgentVariant = agentVariant?"${agentVariant}.${key}":key
+        if ((value instanceof Map) && !(value.keySet().contains('label'))) {
+            traverseAgents(value, newAgentVariant, closure)
+        } else {
+            closure(newAgentVariant, value)
+        }
+    }
+}
+
+
+def agentsTable(Map it){	
+    def flattenedArray = it.flattenedAgentsParam.split(',')
+    def flattenedIdx = 0
+    traverseAgents(it.Agents, "") { variants, _agent ->
+        _agent.label      = (flattenedIdx>=flattenedArray.size())?:flattenedArray[flattenedIdx++]
+        _agent.workdir    = (flattenedIdx>=flattenedArray.size())?:flattenedArray[flattenedIdx++]
+        println "${variants}: label ${_agent.label} workdir: ${_agent.workdir}"
+        if(!_agent.workdir || _agent.workdir?.trim().isEmpty())
+            error("Workdir at Agent $_agent CANNOT BE NULL. Stopping the build early...")        
+    }
+}
